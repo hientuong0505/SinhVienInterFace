e0fd46ae9df3ff969f358839ae53b4e8b6c72ed9
hientuong0505 <hientuong4321@gmail.com>
2021-03-23 18:59:11 +0700

    updated

--- WinformBuoi1/Program.cs
+++ WinformBuoi1/Program.cs
@@ -17,10 +17,6 @@ namespace WinformBuoi1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frm_DANGNHAP());
-            //Application.Run(new frm_NhapMang());
-            //Application.Run(new frm_QLSinhVien());
-            //Application.Run(new frm_GiaiPTBac2());
-            //Application.Run(new frm_TIMTHU());
         }
     }
 }
